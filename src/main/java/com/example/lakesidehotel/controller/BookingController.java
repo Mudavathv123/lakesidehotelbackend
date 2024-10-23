f73353d64e90c16c32c99b455e74f7b16673c58d
Mudavathv123 <mudavathvinod49@gmail.com>
2024-10-23 14:26:44 +0530

    add h2 database

--- src/main/java/com/example/lakesidehotel/controller/BookingController.java
+++ src/main/java/com/example/lakesidehotel/controller/BookingController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,7 @@ import com.example.lakesidehotel.service.IBookingService;
 
 @RestController
 @RequestMapping("/bookings")
-@CrossOrigin(origins = "http://localhost:5173")
+// @CrossOrigin(origins = "http://localhost:5173")
 public class BookingController {
 
     @Autowired
--- src/main/java/com/example/lakesidehotel/controller/RoomController.java
+++ src/main/java/com/example/lakesidehotel/controller/RoomController.java
@@ -1,13 +1,11 @@
 package com.example.lakesidehotel.controller;
 
-import com.example.lakesidehotel.repository.RoomResponse;
-import com.example.lakesidehotel.service.IRoomService;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,9 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.lakesidehotel.repository.RoomResponse;
+import com.example.lakesidehotel.service.IRoomService;
+
 @RestController
 @RequestMapping("/rooms")
-@CrossOrigin(origins = "http://localhost:5173")
+// @CrossOrigin(origins = "http://localhost:5173")
 public class RoomController {
 
     @Autowired
